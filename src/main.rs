b7de83bac3324ac374e4d933eb880b5e1805df15
technolapin <clement.chomicki@protonmail.com>
2019-11-14 15:18:37 +0100

    On peut maintenant récupérer les composants et les modifier

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,95 @@
 extern crate sdl2;
 use anymap::AnyMap;
 use std::collections::HashMap;
+use std::any::{TypeId, Any};
+
+
+/**
+   Used to manage the entities generations and indexes
+**/
+struct EntityAllocator
+{
+    free_indexes: Vec<usize>,
+    max_index: usize,
+    generation: u64
+}
+
+impl EntityAllocator
+{
+    fn new() -> Self
+    {
+        Self
+        {
+            free_indexes: vec![],
+            max_index: 0,
+            generation: 0
+        }
+    }
+    
+    fn new_entity(&mut self) -> Entity
+    {
+        self.generation += 1;
+        match self.free_indexes.pop()
+        {
+            None =>
+            {
+                self.max_index += 1;
+                Entity
+                {
+                    generation: self.generation-1,
+                    index: self.max_index-1
+                }
+            },
+            Some(index) =>
+            {
+                Entity
+                {
+                    generation: self.generation-1,
+                    index: index
+                }
+            }
+        }
+    }
+}
+
+
+struct StorageIndex
+{
+    index: usize
+}
 
 
 struct Entity
 {
-    id: u64,
+    /// the "identity card" of the entity
+    generation: u64,
+    
+    /// the index the components are stored at
     index: usize
 }
 
+
+/// A component is something you store in a storage
 trait Component
 {
     type Storage: Storage;
 }
 
-
+/// A storage is something you store components into
 trait Storage
 {
     type Component: Component;
     fn get(&self, entity: Entity) -> Option<&Self::Component>;
-    fn insert(&mut self, entity: Entity, comp: Self::Component); // should add an Ok(()) return type later
+    fn get_mut(&mut self, entity: Entity) -> Option<&mut Self::Component>;
+    fn insert(&mut self, entity: &Entity, comp: Self::Component); // should add an Ok(()) return type later
     fn new() -> Self;
 }
 
-struct VecStorage<T>    
+// exemple de storage
+struct VecStorage<T>
 {
     free_space: Vec<usize>,
-    data: Vec<T>,
-    
+    data: Vec<Option<T>>
 }
 
 
@@ -36,7 +98,8 @@ where
     Compo: Component
 {
     type Component = Compo;
-    fn get(&self, entity: Entity) -> Option<&Self::Component>
+    
+    fn get(&self, entity: Entity) -> Option<&Compo>
     {
         let index = entity.index;
         if index >= self.data.len() || self.data.get(index).is_none()
@@ -44,9 +107,33 @@ where
             panic!("Tried to access a non-existing component");
         }
         
-        self.data.get(index)
+        self.data.get(index).unwrap().as_ref()
     }
-    fn insert(&mut self, entity: Entity, comp: Self::Component){}
+    
+    fn get_mut(&mut self, entity: Entity) -> Option<&mut Compo>
+    {
+        let index = entity.index;
+        if index >= self.data.len() || self.data.get(index).is_none()
+        {
+            panic!("Tried to access a non-existing component");
+        }
+        
+        self.data.get_mut(index).unwrap().as_mut()
+    }
+    
+
+    fn insert(&mut self, entity: &Entity, comp: Self::Component)
+    {
+        if self.data.len() <= entity.index
+        {
+            for i in self.data.len()..(entity.index)
+            {
+                self.data.push(None)
+            }
+            self.data.push(Some(comp));
+        }
+    }
+    
     fn new() -> Self
     {
         Self
@@ -56,6 +143,28 @@ where
         }
     }
 }
+
+struct NotAStorage<Compo>(Vec<Compo>);
+impl<Compo> Storage for NotAStorage<Compo>
+where
+     Compo: Component + 'static
+{
+    type Component = Compo;
+    fn get(&self, entity: Entity) -> Option<&Compo>
+    {
+        None
+    }
+    fn get_mut(&mut self, entity: Entity) -> Option<&mut Compo>
+    {
+        None
+    }
+    fn insert(&mut self, entity: &Entity, comp: Compo){}
+    fn new() -> Self
+    {
+        Self(vec![])
+    }
+}
+
 
 #[derive(Debug)]
 struct Position
@@ -69,27 +178,50 @@ impl Component for Position
     type Storage = VecStorage<Self>;
 }
 
-use std::any::{TypeId, Any};
 
 
-
+/**
+ The structure owning the entities, the storages and everything else
+**/
 struct World
 {
+    entity_allocator: EntityAllocator,
     entities: Vec<Entity>,
     components: AnyMap,
-    components_registry: Vec<TypeId>
 }
 
 impl World
 {
+    fn new() -> Self
+    {
+        Self
+        {
+            entity_allocator: EntityAllocator::new(),
+            entities: vec![],
+            components: AnyMap::new(),
+        }
+        
+    }
+    
     fn add_component<Comp>(&mut self) -> &mut Self
     where
         Comp: Component + 'static
     {
-        self.components_registry.push(TypeId::of::<Comp::Storage>());
         self.components.insert(Comp::Storage::new());
         self
     }
+
+    fn add_entity_component<Comp>(
+        &mut self,
+        entity: &Entity,
+        component: <<Comp as Component>::Storage as Storage>::Component) -> &mut Self
+    where
+        Comp: Component + 'static
+    {
+        self.get_storage_mut::<Comp>().unwrap().insert(entity, component);
+        self
+    }
+
 
     fn get_storage<Comp>(&self) -> Option<&Comp::Storage>
     where
@@ -97,6 +229,21 @@ impl World
     {
         self.components.get::<Comp::Storage>()
     }
+    
+    fn get_storage_mut<Comp>(&mut self) -> Option<&mut Comp::Storage>
+    where
+        Comp: Component + 'static
+    {
+        self.components.get_mut::<Comp::Storage>()
+    }
+
+    fn new_entity(&mut self) -> &mut Self
+    {
+        self.entities.push(self.entity_allocator.new_entity());
+        self
+        
+    }
+    
 }
 
 struct IsFalling;
@@ -106,29 +253,80 @@ impl Component for IsFalling
 }
 
 
+enum Shape
+{
+    Square(i32, i32, u32, u32),
+    Point(i32, i32),
+}
+
+impl Component for Shape
+{
+    type Storage = VecStorage<Self>;
+}
+
+
+
+
+
+
 fn main() {
 
-    let mut world = World
-    {
-        entities: vec![],
-        components: AnyMap::new(),
-        components_registry: vec![]
-    };
+    let mut world = World::new();
 
     
     world
         .add_component::<Position>()
         .add_component::<IsFalling>();
-   
-    println!("{:?}", world.components);
 
-    let st_ref = world.get_storage::<Position>().unwrap();
-
-    for position in st_ref.data.iter()
     {
-        println!("Position {:?}", position);
+        let st_ref = world.get_storage_mut::<Position>().unwrap();
+        println!("lol {:?}", st_ref.data);
+    }
+    {
+        let entity = world.entity_allocator.new_entity();
+        world.add_entity_component::<Position>(&entity, Position{x: 64, y: 32});
+    }
+    {
+        let st_ref = world.get_storage_mut::<Position>().unwrap();
+        println!("lol {:?}", st_ref.data);
+    }
+    {
+        let entity = world.entity_allocator.new_entity();
+        world.add_entity_component::<Position>(&entity, Position{x: 64, y: 32});
+
+    }
+    {
+        let st_ref = world.get_storage_mut::<Position>().unwrap();
+        println!("lol {:?}", st_ref.data);
     }
     
+    println!("{:?}", world.components);
+
+    {
+        let st_ref = world.get_storage_mut::<Position>()
+            .unwrap().data
+            .iter_mut()
+            .map(|y| match y.as_mut() {Some(x) => x, _ => unreachable!()});
+
+        println!("lol {:?}", st_ref);
+        for position in st_ref
+        {
+            position.x = 1;
+            println!("Position {:?}", position);
+        }
+    }
+    {
+        let st_ref = world.get_storage_mut::<Position>()
+            .unwrap().data
+            .iter_mut()
+            .map(|y| match y.as_mut() {Some(x) => x, _ => unreachable!()});
+
+        println!("lol {:?}", st_ref);
+        for position in st_ref
+        {
+            println!("Position {:?}", position);
+        }
+    }
 
 }
 
