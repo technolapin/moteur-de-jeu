f4a5f15d30d6bd419486deebcd881ecb6bb32a6d
BobbyLaMalice <56543229+BobbyLaMalice@users.noreply.github.com>
2020-01-23 13:03:59 +0100

    Update main.rs

--- test_ui2/src/main.rs
+++ test_ui2/src/main.rs
@@ -1,68 +1,26 @@
-extern crate sdl2;
+extern crate glium;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use std::collections::HashSet;
-use sdl2::mouse::MouseButton;
-use std::time::Duration;
+fn main() {
+    use glutin::{MouseButton,ElementState,VirtualKeyCode,KeyboardInput};
+    let mut events_loop = glutin::EventsLoop::new();
+    let wb = glutin::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let _display = glium::Display::new(wb, cb, &events_loop).unwrap();
 
+    let mut closed = false;
+    while !closed {
+        events_loop.poll_events(|ev| {
+            match ev {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::MouseInput{device_id,state: ElementState::Pressed,button,modifiers} => {println!("Boutons Souris Appuyé  {:?} ",button)},
+                    glutin::WindowEvent::MouseWheel{device_id,delta,phase,modifiers} => {println!("Boutons Souris Appuyé  {:?} ",delta)},
+                    glutin::WindowEvent::KeyboardInput{device_id,input: KeyboardInput{scancode: u32,state: ElementState::Pressed,virtual_keycode,modifiers}} => {println!("Boutton Clavier Appuyé  {:?}",virtual_keycode)},
+                    glutin::WindowEvent::CloseRequested => {closed = true;println!("Fermeture fenêtre")},
 
-// Listener pour clavier (le focus doit être dans la fenêtre)
-pub fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-
-    // Zone d'action du listener
-    let _window = video_subsystem.window("Keyboard", 800, 600)
-        .position_centered()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let mut events = sdl_context.event_pump()?;
-    let mut state;
-    let mut prev_keys = HashSet::new();
-
-    'running: loop {
-        for event in events.poll_iter() {
-            if let Event::Quit {..} = event {
-                break 'running;
-            };
-        }
-
-        //Partie Clavier
-
-        // Create a set of pressed Keys.
-        let keys = events.keyboard_state().pressed_scancodes().filter_map(Keycode::from_scancode).collect();
-
-        // Get the difference between the new and old sets.
-        let new_keys = &keys - &prev_keys; // Touche récement appuyé
-        let old_keys = &prev_keys - &keys; // Garde en mémoire les touches maintenues pressé
-
-        if !new_keys.is_empty(){
-            println!("J'appui : {:?}", new_keys);
-            //if new_keys.contains("Q"){
-             //   println!("Je veux sortir !");
-            //}
-        }
-
-        if !old_keys.is_empty() {
-            println!("Je relache : {:?}", old_keys);
-        }
-
-        prev_keys = keys;
-
-
-        //Partie Souris
-
-        // Event sur Clic droit ou gauche
-        if events.mouse_state().is_mouse_button_pressed(MouseButton::Left) | events.mouse_state().is_mouse_button_pressed(MouseButton::Right){
-            state = events.relative_mouse_state();
-            println!("Relative - X = {:?}, Y = {:?}", state.x(), state.y()); // Déplacement de souris
-            println!("State - X = {:?}, Y = {:?}", events.mouse_state().x(), events.mouse_state().y()); // Position de souris
-        }
-
-        std::thread::sleep(Duration::from_millis(100));
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
     }
-
-    Ok(())
 }
