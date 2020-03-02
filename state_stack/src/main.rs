29fe6c154a2fe985411a14fa05fb18d4adfc43cb
734F96 <50321866+734F96@users.noreply.github.com>
2020-03-02 15:34:18 +0100

    Seconde version de la pile d'états

--- state_stack/src/main.rs
+++ state_stack/src/main.rs
@@ -6,32 +6,56 @@ pub enum States {
 	Game,
 	PauseMenu,
 	Parametre,
-	None, // Par défaut
+	EtatDeBase, // Etat forcément en bas
+	None, // Par défaut, n'est utilisé que s'il n'y a pas d'état courant
+}
+
+pub enum StateContainer {
+	ActiveState ( States ),
+	InactiveState ( States ),
 }
 
 pub struct CurrentState {
 	// Il n'y a pas de pile en Rust, on utilise un vecteur
-	stack_of_states : Vec<States>,
+	stack_of_states : Vec<StateContainer>,
 }
 
 impl CurrentState {
     pub fn new() -> Self {
-    	return Self{ stack_of_states : Vec::new() };
+    	return Self{ stack_of_states : vec![ StateContainer::ActiveState( States::EtatDeBase ) ] };
     }
     
-    pub fn push_state( mut self, state : States ) {
-    	self.stack_of_states.push( state );
+    // Note : Travailler par référence, donc mettre &
+    // Ca ne change rien au code dans la fonction, ni à l'utilisation externe
+    pub fn push_state( &mut self, state : States ) {
+    	self.stack_of_states.push( StateContainer::ActiveState( state ) );
     }
     
-    pub fn get_current_state( self ) -> States {
+    pub fn get_current_state( &self ) -> States {
     	return match self.stack_of_states.last() {
-    		Some( state ) => *state,
-    		_ => States::None
+    		Some( state_container ) => match state_container {
+    			StateContainer::ActiveState ( state ) | StateContainer::InactiveState( state ) => *state
+			},
+			_ => States::None // Ne devrait jamais arriver
     	};
     }
     
-    pub fn pop_state( mut self ) {
-    	self.stack_of_states.pop();
+    pub fn is_current_state_active( &self ) -> bool {
+    	return match self.stack_of_states.last() {
+    		Some( state_container ) => match state_container {
+    			StateContainer::ActiveState ( _state ) => true,
+    			StateContainer::InactiveState ( _state ) => false
+    		},
+    		_ => false // Ne devrait jamais arriver
+    	};
+    }
+    
+    pub fn pop_state( &mut self ) {
+    	if self.stack_of_states.len() > 1 { // Il faut que EtatDeBase reste en bas
+    		if !self.is_current_state_active() {
+    			self.stack_of_states.pop();
+    		}
+    	}
     }
 }
 
